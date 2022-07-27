e3457b606ca93928799ead5fc8a0af9685335e70
Ongidi Geofrey <ongidigeofrey@gmail.com>
2022-07-27 15:55:39 -0400

    comment login user after registration in RegisteredUserController

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -48,7 +48,7 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
+        //Auth::login($user);
 
         return  redirect('login');//redirect(RouteServiceProvider::HOME);
     }
